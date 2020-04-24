5e4c6fcd879c4c184d1c68e64e1d9ca97003e7bc
owenabbott <ocabbott@gmail.com>
2020-04-24 10:56:53 -0500

    Done.

--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,3 +1,7 @@
 class Landmark < ActiveRecord::Base
   # add relationships here
+  belongs_to :figure
 end
+
+
+# - A landmark `belongs_to` a figure and a figure `has_many` landmarks.
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,3 +1,10 @@
 class Title < ActiveRecord::Base
   # add relationships here
+  has_many :figure_titles
+  has_many :figures, through: :figure_titles
 end
+
+# - A landmark `belongs_to` a figure and a figure `has_many` landmarks.
+# - Titles and figures have a "many-to-many" relationship, so we'll need a join table.
+
+#has_many :titles, through: :join table
